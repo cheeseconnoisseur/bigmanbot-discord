31220a9fdbcc7c20197093de2c3e6f4d0d9a06a8
cheeseconnoisseur <arturo.dezon@gmail.com>
2018-01-15 20:42:19 +0000

    got rid of calander for public version

--- bigman.py
+++ bigman.py
@@ -32,18 +32,6 @@ async def on_message(message):
     if message.content.startswith('!gay'):
         UserID = message.author.id
         await client.send_message(message.channel,"<@{}>".format(UserID))
-    if message.content.startswith('!nextcal'):
-        UserID = message.author.id
-        maincal(1)
-        kay = []
-        with open('temp.txt','r') as f:
-            kay = f.read()
-            kay = kay.split("\n")
-            f.close()
-            os.remove('temp.txt')
-            print(kay)
-            event = str(kay[0])
-            await client.send_message(message.channel,"<@{}> next event {}".format(UserID, event))
     if message.content.startswith('!farth'):
         UserID = message.author.id
         await client.send_message(message.channel,"<@{}> shut up you rabient homosexual".format(UserID))
@@ -68,26 +56,6 @@ async def on_message(message):
     if message.content.startswith('!saygay'):
         args = message.content.split(" ")
         await client.send_message(message.channel, "{}".format(" ".join(args[1:])))
-
-    if message.content.startswith('!cal'):
-        UserID = message.author.id
-        if UserID == '310469854564057088':
-            args = message.content.split(" ")
-            args = int(args[1])
-            maincal(args)
-            kay = []
-            with open('temp.txt','r') as f:
-                kay = f.read()
-                kay = kay.split("\n")
-                f.close()
-                os.remove('temp.txt')
-                print(kay)
-            for event in reversed(kay[:-1]):
-                print(kay)
-                event = str(event)
-                await client.send_message(message.channel,"<@{}> next event {}".format(UserID, event))
-        else:
-            await client.send_message(message.channel,"<@{}> and who the hell are you snooping in my calander".format(UserID))
 
 
 
